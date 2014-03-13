9165368d5d1110e72a1e1c873e619bae76102733
Thomas Bennett <thomas@ska.ac.za>
2014-03-13 15:30:28 +0200

    On branch basic Changes to be committed: 	new file:   katsdpdata/met_extractors.py 	new file:   scripts/katfile_met_extractor.py 	new file:   scripts/rts_met_extractor.py
    These are the metadata extractors for kat-7 and rts respectively

---
+++ scripts/katfile_met_extractor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import katdal
+import os
+import sys
+import subprocess
+import time
+
+from katsdpdata.met_extractors import KatFileMetExtractor
+from optparse import OptionParser
+from xml.etree import ElementTree
+#from xml.dom import minidom
+
+usage = 'Usage: %prog filename'
+parser = OptionParser(usage=usage)
+(options, args) = parser.parse_args()
+
+if (len(sys.argv) == 2):
+    filename = sys.argv[1]
+else:
+    print parser.format_help()
+    sys.exit(0)
+
+metfilename = '%s.%s' % (filename, 'met')
+if os.path.isfile(metfilename):
+    print 'Metadate file %s already exists.' % (metfilename)
+    sys.exit(0)
+
+#met extractor specific
+katdata = katdal.open(os.path.abspath(filename))
+major_version = int(float(katdata.version))
+met_extractor = KatFileMetExtractor(katdata)
+met_extractor.set_metadata()
+
+with open(metfilename, 'w') as metfile:
+   metfile.write(met_extractor.get_metadata())
+
