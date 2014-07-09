d0ad437f018c10512f9a712b51113cbdffc56841
Thomas Bennett <thomas@lmzxq-mbp.local>
2014-07-09 14:49:57 +0200

    Change tabs to spaces

--- katsdpdata/test/test_parsing_date_range.py
+++ katsdpdata/test/test_parsing_date_range.py
@@ -4,28 +4,28 @@ import time
 from katsdpdata import KatSdpSolrClient
 
 class TestParsingDateRange(unittest.TestCase):
-	def setUp(self):
-		self.sdptest = KatSdpSolrClient('junk')
+    def setUp(self):
+        self.sdptest = KatSdpSolrClient('junk')
 
-	def test__SAST_to_ISO8601(self):
-		test_input = '1/1/2001 00:00:00 SAST'
-		test_result = self.sdptest._SAST_to_ISO8601(test_input)
-		self.assertEqual(test_result, '2000-12-31T22:00:00Z')
+    def test__SAST_to_ISO8601(self):
+        test_input = '1/1/2001 00:00:00 SAST'
+        test_result = self.sdptest._SAST_to_ISO8601(test_input)
+        self.assertEqual(test_result, '2000-12-31T22:00:00Z')
 
-	def test__date_to_ISO8601(self):
-		test_input = '1/1/2001'
-		test_result = self.sdptest._date_to_ISO8601(test_input)
-		self.assertEqual(test_result, '2000-12-31T22:00:00Z')
+    def test__date_to_ISO8601(self):
+        test_input = '1/1/2001'
+        test_result = self.sdptest._date_to_ISO8601(test_input)
+        self.assertEqual(test_result, '2000-12-31T22:00:00Z')
 
-	def test__parse_date_range(self):
-		test_input = '1/1/2001 to 31/12/2001'
-		test_result = self.sdptest._parse_date_range(test_input)
-		self.assertEqual(test_result, '[2000-12-31T22:00:00Z TO 2001-12-30T22:00:00Z]')
+    def test__parse_date_range(self):
+        test_input = '1/1/2001 to 31/12/2001'
+        test_result = self.sdptest._parse_date_range(test_input)
+        self.assertEqual(test_result, '[2000-12-31T22:00:00Z TO 2001-12-30T22:00:00Z]')
 
-		test_input = '[1/1/2001 to 31/12/2001]'
-		test_result = self.sdptest._parse_date_range(test_input)
-		self.assertEqual(test_result, '[2000-12-31T22:00:00Z TO 2001-12-30T22:00:00Z]')
+        test_input = '[1/1/2001 to 31/12/2001]'
+        test_result = self.sdptest._parse_date_range(test_input)
+        self.assertEqual(test_result, '[2000-12-31T22:00:00Z TO 2001-12-30T22:00:00Z]')
 
-		test_input = 'now-7day to now'
-		test_result = self.sdptest._parse_date_range(test_input)
-		self.assertEqual(test_result, '[NOW-7DAY TO NOW]')
+        test_input = 'now-7day to now'
+        test_result = self.sdptest._parse_date_range(test_input)
+        self.assertEqual(test_result, '[NOW-7DAY TO NOW]')
