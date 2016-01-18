4d44fc6ae9ef2d070ec4ad6caf010a7ccb1c019b
Thomas Bennett <thomas@ska.ac.za>
2016-01-18 15:05:59 +0200

    Bugfix: remove option for md5-calc. Always on. Remove unused commandline options

--- katsdptransfer/scripts/sun_store_transfer_prod.py
+++ katsdptransfer/scripts/sun_store_transfer_prod.py
@@ -15,16 +15,10 @@ def get_options():
     parser = OptionParser(usage=usage)
     parser.add_option('--filename', type='str',
         help='The name of the file to transfer.')
-    parser.add_option('--calc-md5', action='store_true', default=False,
-        help='On the fly caclulate md5 checksum and transfer to the sunstore.')
-    parser.add_option('--delete', action='store_true', default=False,
-        help='Delete the file after successful transfer to the sunstore.')
-    parser.add_option('--debug', action='store_true', default=False,
-        help='Set logging level to debug.')
     (options, args) = parser.parse_args()
     return options
 
 opts = get_options()
 logging.basicConfig(level=logging.DEBUG if opts.debug else logging.INFO, format='%(asctime)s %(levelname)s %(message)s', handlers=[logging.StreamHandler()])                
-transferer = katsdptransfer.SunStoreTransferFile(filename=opts.filename, tx_md5=opts.calc_md5)
+transferer = katsdptransfer.SunStoreTransferFile(filename=opts.filename)
 transferer.run()
