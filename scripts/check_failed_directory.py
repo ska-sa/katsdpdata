8407864d71a4b3b3fdce6de0bf745b6630e323f9
Thomas Bennett <thomas@ska.ac.za>
2017-03-06 13:22:41 +0200

    Do some stuff before try/except

--- scripts/check_failed_directory.py
+++ scripts/check_failed_directory.py
@@ -1,21 +1,31 @@
+#!/usr/bin/env python
 import re
 import os
 import katdal
 import katsdpdata
 import time
 import logging
+import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger("katdal").setLevel(logging.ERROR)
 
+staging_dir = '/var/kat/archive/data/ftp/staging'
 failed_dir = '/var/kat/archive/data/ftp/failed/'
+
 h5files = [os.path.join(failed_dir,f) for f in os.listdir(failed_dir) if os.path.isfile(os.path.join(failed_dir, f)) and re.match('[0-9]{10}.h5$', f)]
+
+#move md5s
+for h5 in h5files:
+    h5_md5 = os.path.basename(h5) + '.md5'
+    if os.path.isfile(os.path.join(staging_dir, h5_md5)):
+        os.rename(os.path.join(staging_dir, h5_md5), os.path.join(failed_dir, h5_md5))
 
 katdal_errors = {}
 katdal_pass = []
 for h5 in h5files:
     ctime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getctime(h5)))
     fsize = os.path.getsize(h5)
-    try:                                                                                                           
+    try:
         k = katdal.open(h5)
         katdal_pass.append(h5)
     except Exception, e:
@@ -28,7 +38,9 @@ for h5 in h5files:
 met_extractor_error = {}
 met_extractor_pass = []
 for h5 in katdal_pass:
-    try:                                                                                                           
+    ctime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getctime(h5)))
+    fsize = os.path.getsize(h5)
+    try:
         km = katsdpdata.met_extractors.TelescopeProductMetExtractor.factory(h5)
         met_extractor_pass.append(km)
     except Exception, e:
@@ -47,7 +59,7 @@ for key,val in katdal_errors.iteritems():
     for v in sorted(val, reverse=True):
         print '%s: %s %i bytes' % (v[0], v[1], v[2])
 print '+'*len(title) + '\n'
- 
+
 title = 'MET EXTRACTOR ERROR'
 print title
 print '+'*len(title)
