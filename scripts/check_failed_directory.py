94e2b1acd4cb29885318206351a109a686827b66
Thomas Bennett <thomas@ska.ac.za>
2017-03-13 14:17:18 +0200

    Major refactoring of code - main mode and daemon mode

--- scripts/check_failed_directory.py
+++ scripts/check_failed_directory.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import re
 import os
+import sys
 import katdal
 import katsdpdata
 import logging
+import time
 from datetime import datetime, timedelta
 
 from optparse import OptionParser
@@ -14,7 +16,7 @@ default_dirs = []
 default_dirs.append('/var/kat/archive/data/ftp/failed')
 default_dirs.append('/var/kat/archive2/data/ftp/failed')
 
-wrapper = '<html><head><link type="text/css" href="http://kat-archive.kat.ac.za:8080/archive_search/css/ska_style.css" rel="stylesheet"><title>%s</title></head><body><div align="center">%s</div></body></html>'
+htaccess_file = '.htaccess'
 
 def get_options():
     """Sets options from the arguments passed to the script.
@@ -28,49 +30,68 @@ def get_options():
 
     parser = OptionParser(usage=usage)
     parser.add_option('--dir', type='str',
-         help='Directory to check. Use --archive --archive2 to check default directories.')
-    parser.add_option('-a', '--archive', action='store_true', default=False,
-         help='Check directory, /var/kat/archive/data/ftp/failed')
-    parser.add_option('-b', '--archive2', action='store_true', default=False,
-         help='Check directory, /var/kat/archive2/data/ftp/failed')
+         help='Directory to check. Overrides default directories checked.')
     parser.add_option('--hours', type=float,
-         help='Only look at the last .. hours.')
-    parser.add_option('--web', action='store_true', default=False,
-         help='Output as html')
+         help='Only look at the last number of hours requested.')
+    parser.add_option('--daemon', action='store_true', default=False,
+         help='Loop forever and update %s' % (htaccess_file))
     (options, args) = parser.parse_args()
 
     return options
 
-def perform_checks(failed_dir, cutoff=None):
-    h5files = [os.path.join(failed_dir,f) for f in os.listdir(failed_dir) if os.path.isfile(os.path.join(failed_dir, f)) and re.match('[0-9]{10}.h5$', f)]
-    #move md5s
-    if failed_dir in default_dirs:
-        staging_dir = os.path.join(os.path.split(os.path.normpath(failed_dir))[0], 'staging') 
-        for h5 in h5files:
-            h5_md5 = os.path.basename(h5) + '.md5'
-            if os.path.isfile(os.path.join(staging_dir, h5_md5)):
-                os.rename(os.path.join(staging_dir, h5_md5), os.path.join(failed_dir, h5_md5))
+def get_h5files(directory):
+    return [os.path.join(directory,f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and re.match('[0-9]{10}.h5$', f)]
 
+def filter_htaccess_h5files(directory, htfile, h5files):
+    keep_descriptions = []
+    try:
+        with open(htfile, 'r') as htaccess:
+            info = htaccess.read()
+            for i in info.split('\n'):
+                if re.match('AddDescription.*[0-9]{10}.h5$', i):
+                    i_file = re.findall('[0-9]{10}.h5$', i)[0]
+                    if os.path.join(directory, i_file) in h5files:
+                        h5files.remove(os.path.join(directory, i_file))
+                        keep_descriptions.append(i)
+    except IOError:
+        print 'No %s in %s' % (htaccess_file, directory)
+    return h5files, keep_descriptions
+
+def filter_cutoff(cutoff_time, h5files):
+    for h5 in h5files:
+        ctime = datetime.fromtimestamp(os.path.getctime(h5))
+        if cutoff_time and ctime < cutoff_time:
+            h5files.remove(h5)
+    return h5files
+
+def move_md5s(failed_dir, h5files):
+    staging_dir = os.path.join(os.path.split(os.path.normpath(failed_dir))[0], 'staging')
+    for h5 in h5files:
+        h5_md5 = os.path.basename(h5) + '.md5'
+        if os.path.isfile(os.path.join(staging_dir, h5_md5)):
+            os.rename(os.path.join(staging_dir, h5_md5), os.path.join(failed_dir, h5_md5))
+
+def check_katdal(h5files):
     katdal_errors = {}
     katdal_pass = []
     for h5 in h5files:
         ctime = datetime.fromtimestamp(os.path.getctime(h5))
-        if cutoff and ctime < cutoff:
-           continue 
         fsize = os.path.getsize(h5)
         try:
             k = katdal.open(h5)
             katdal_pass.append(h5)
         except Exception, e:
-            e = repr(e)
+            e = repr(e).replace('"','\'')
             if katdal_errors.has_key(e):
                 katdal_errors[e].append((ctime.isoformat(), os.path.basename(h5), fsize),)
             else:
                 katdal_errors[e] = [(ctime.isoformat(), os.path.basename(h5), fsize),]
+    return katdal_errors, katdal_pass
 
+def check_met_extractor(h5files):
     met_extractor_errors = {}
     met_extractor_pass = []
-    for h5 in katdal_pass:
+    for h5 in h5files:
         ctime = datetime.fromtimestamp(os.path.getctime(h5))
         fsize = os.path.getsize(h5)
         try:
@@ -82,68 +103,96 @@ def perform_checks(failed_dir, cutoff=None):
                 met_extractor_errors[e].append((ctime.isoformat(), os.path.basename(h5), fsize),)
             else:
                 met_extractor_errors[e] = [(ctime.isoformat(), os.path.basename(h5), fsize),]
-    return katdal_errors, met_extractor_errors, met_extractor_pass
+    return met_extractor_errors, met_extractor_pass
 
+def create_descriptions(error_type, errors):
+    descriptions = []
+    for key,val in errors.iteritems():
+        for v in sorted(val, reverse=True):
+            descriptions.append('AddDescription "%s: %s" %s' % (error_type, key, v[1]))
+    return descriptions
 
-def html_results(failed_dir, katdal_errors, met_extracor_errors, met_extractor_pass):
-    with open(os.path.join(failed_dir, 'index.html'), 'w') as index:
-        body = html_body(katdal_errors, met_extracor_errors, met_extractor_pass)
-        html = wrapper % (failed_dir, '\n'.join(body))
-        index.write(html)
-  
-def html_body(katdal_errors, met_extractor_errors, met_extractor_pass):
-    th = '<tr><th>%s</th></tr>'
-    td = '<tr><td>%s</td/></tr>'
-    tb_start = '<table width="950" cellspacing="10" cellpadding="0" border="0" bgcolor="#ffffff">'
-    tb_end = '</table>'
-    body = []
-    body.append(tb_start)
-    body.append(th % 'katdal errors')
-    body.append(tb_end)
+def write_htaccess(htfile, descriptions):
+    with open(htfile, 'w') as htaccess:
+        htaccess.write('IndexOptions FancyIndexing\n')
+        htaccess.write('IndexIgnore *.md5\n')
+        htaccess.write('AddDescription "CAS metadata" *.met\n')
+        htaccess.write('\n'.join(descriptions))
+
+def update_h5files_htaccess_descriptions(directory):
+    h5files = get_h5files(directory)
+    h5files, descriptions = filter_htaccess_h5files(directory, os.path.join(directory, htaccess_file), h5files)
+    move_md5s(directory,h5files)
+    katdal_errors, katdal_pass = check_katdal(h5files)
+    met_extractor_errors, met_extractor_pass = check_met_extractor(katdal_pass)
+    descriptions.extend(create_descriptions('katdal error', katdal_errors))
+    descriptions.extend(create_descriptions('met error', met_extractor_errors))
+    return descriptions
+
+def update_htaccess():
+    for d in default_dirs:
+        descriptions = []
+        descriptions.extend(update_h5files_htaccess_descriptions(d))
+        write_htaccess(os.path.join(d,htaccess_file), descriptions)
+
+     
+def show_h5_results(directory, katdal_errors, met_extractor_errors, met_extractor_pass):
+    print directory
+    title = 'KATDAL ERRORS'
+    print title
+    print '+'*len(title)
     for key,val in katdal_errors.iteritems():
-        body.append(tb_start)
-        body.append(th % key)
+        print key
+        print '='*len(key)
         for v in sorted(val, reverse=True):
-            body.append(td % ('%s: %s %i bytes' % (v[0], v[1], v[2])))
-        body.append(tb_end) 
-    
-    body.append(tb_start)
-    body.append(th % 'TelescopeProductMetExtractor errors')
-    bodr.append(tb_end)
+            print '%s: %s %i bytes' % (v[0], v[1], v[2])
+    print '+'*len(title) + '\n'
+
+    title = 'MET EXTRACTOR ERRORS'
+    print title
+    print '+'*len(title)
     for key,val in met_extractor_errors.iteritems():
-        body.append(tb_start) 
-        body.append(th % key)
+        print key
+        print '='*len(key)
         for v in sorted(val, reverse=True):
-            body.append(td % ('%s: %s %i bytes' % (v[0], v[1], v[2])))
-        body.append(tb_end)
+            print '%s: %s %i bytes' % (v[0], v[1], v[2])
+    print '+'*len(title) + '\n'
 
-    body.append(tb_start)
-    body.append(th % 'Passed all test')
-   
+    title = 'FILES STILL PASSING'
+    print title
+    print '+'*len(title)
     if met_extractor_pass:
-        body.append('\n'.join(td % (met_extractor_pass)))
+        print '\n'.join(met_extractor_pass)
     else:
-        body.append(td % 'No files remaining.')
-    body.append(tb_end)
-    return body
+        print 'No files remaining.'
+    print '+'*len(title) + '\n'
 
-opts = get_options()
+def run_daemon():
+    while True:
+        print 'Updating...'
+        update_htaccess()
+        print 'done.'
+        time.sleep(10)
 
-failed_dirs = []
-if opts.dir:
-    failed_dirs.append(opts.dir)
-if opts.archive:
-    failed_dirs.append(default_dirs[0])
-if opts.archive2:
-    failed_dirs.append(default_dirs[1])
-if opts.hours:
-    cutoff = datetime.now() - timedelta(hours=opts.hours)
-else:
-    cutoff = None
+def run_main(cutoff):
+    for d in default_dirs:
+        h5files = get_h5files(d)
+        h5files = filter_cutoff(cutoff, h5files)
+        katdal_errors, katdal_pass = check_katdal(h5files)   
+        met_errors, met_pass = check_met_extractor(katdal_pass)  
+        show_h5_results(d, katdal_errors, met_errors, met_pass)         
 
-for failed_dir in failed_dirs:
-    katdal_errors, met_extractor_errors, met_extractor_pass = perform_checks(failed_dir, cutoff)
-    if opts.web:    
-        html_results(failed_dir, katdal_errors, met_extractor_errors, met_extractor_pass)
+if __name__ == '__main__':
+    opts = get_options()
+
+    if opts.daemon:
+        run_daemon()
+        sys.exit()
+    if opts.dir:
+        default_dirs = [opts.dir]
+    if opts.hours:
+        cutoff = datetime.now() - timedelta(hours=opts.hours)
     else:
-        print html_body(katdal_errors, met_extractor_errors, met_extractor_pass)
+        cutoff = None
+    run_main(cutoff)
+
