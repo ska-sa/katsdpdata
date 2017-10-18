4502ff39a8e09b915b75877b131438d363c5d3eb
Bruce Merry <bmerry@ska.ac.za>
2017-10-18 12:29:40 +0200

    Switch to using spead2 1.4.0b1 release
    This replaces the hacky install from github.

--- katsdpfilewriter/setup.py
+++ katsdpfilewriter/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'h5py',
         'numpy',
-        'spead2>=0.5.0',
+        'spead2>=1.4.0b1',
         'katcp',
         'katdal',
         'katsdptelstate',
