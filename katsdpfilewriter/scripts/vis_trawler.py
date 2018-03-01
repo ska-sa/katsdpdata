e809ae5818e7d64a722d73f3e921ee77904141b2
Thomas Bennett <thomas@lmzxq-mbp.local>
2018-03-01 11:33:26 +0200

    Cut and paste from met_writer.py s3 functions with modifications: (1) remove auth/secret keys from boto_dict, these are taken from ~/.aws/credentials. (2) get s3 conn info from s3_conn object. (3) reformat all string printing for python3.:

--- katsdpfilewriter/scripts/vis_trawler.py
+++ katsdpfilewriter/scripts/vis_trawler.py
@@ -90,6 +90,47 @@ def parallel_upload(file_list, x, **kwargs):
         executor.shutdown(wait=True)
     return [f.result() for f in future]
 
+#cut and paste with modification from katsdpmetawriter/scripts/meta_writer.py
+def make_boto_dict(s3_args):
+    """Create a dict of keyword parameters suitable for passing into a boto.connect_s3 call using the supplied args."""
+    return {
+            "host": s3_args.s3_host,
+            "port": s3_args.s3_port,
+            "is_secure": False,
+            "calling_format": boto.s3.connection.OrdinaryCallingFormat()
+           }
+
+#cut and paste with modification from katsdpmetawriter/scripts/meta_writer.py
+def get_s3_connection(boto_dict, fail_on_boto=False):
+    """Test the connection to S3 as described in the args, and return
+    the current user id and the connection object.
+    In general we are more concerned with informing the user why the
+    connection failed, rather than raising exceptions. Users should always
+    check the return value and make appropriate decisions.
+    If set, fail_on_boto will not suppress boto exceptions. Used when verifying
+    credentials.
+    Returns
+    -------
+    s3_conn : S3Connection
+        A connection to the s3 endpoint. None if a connection error occurred.
+    """
+    s3_conn = boto.connect_s3(**boto_dict)
+    try:
+        s3_conn.get_canonical_user_id()
+         # reliable way to test connection and access keys
+        return s3_conn
+    except socket.error as e:
+        logger.error("Failed to connect to S3 host {}:{}. Please check network and host address. ({})".format(s3_conn.host, s3_conn.host.port, e))
+    except boto.exception.S3ResponseError as e:
+        if e.error_code == 'InvalidAccessKeyId':
+            logger.error("Supplied access key {} is not for a valid S3 user.".format(s3_conn.access_key))
+        if e.error_code == 'SignatureDoesNotMatch':
+            logger.error("Supplied secret key is not valid for specified user.")
+        if e.status == 403 or e.status == 409:
+            logger.error("Supplied access key ({}) has no permissions on this server.".format(s3_conn.access_key))
+        if fail_on_boto:
+            raise
+    return None
 
 if __name__ == "__main__":
     katsdpservices.setup_logging()
