5414bea5eaa170852e81d16a514b543d74a6afb0
Thomas Bennett <thomas@ska.ac.za>
2018-03-26 10:09:25 +0200

    Add in basic download of products

--- katsdpdata/prod_handler.py
+++ katsdpdata/prod_handler.py
@@ -5,20 +5,15 @@ except ImportError:
 import logging
 import multiprocessing
 import os
-import re
-import sys
+import pysolr
 import socket
-import shutil
 import time
 
 import boto
 import boto.s3.connection
 
-import katsdpservices
-
-import katsdpdata.meerkat_product_extractors
-import katsdpdata.met_handler
-import katsdpdata.met_detectors
+import met_handler
+import met_extractors
 
 
 logger = logging.getLogger(__name__)
@@ -86,61 +81,6 @@ def get_s3_connection(boto_dict):
 
 
 
-def ingest_vis_product(trawl_dir, prod_id, original_refs, prod_met_extractor, solr_url, boto_dict):
-    """Ingest a product into the archive. This includes extracting and uploading
-    metadata and then moving the product into the archive.
-
-    Parameters
-    ----------
-    trawl_dir: string : full path to directory to trawl for ingest product.
-    prod_id: string : unique id for the product.
-    original_refs : list : list of product file(s).
-    product_met_extractor: class : a metadata extractor class.
-    solr_url: string : sorl endpoint for metadata queries and upload.
-
-    Returns
-    -------
-    met : dict : a metadata dictionary with uploaded key:value pairs.
-    """
-    try:
-        pm_extractor = prod_met_extractor(original_refs[0])
-        pm_extractor.extract_metadata()
-    except Exception as err:
-        bucket_name = os.path.relpath(original_refs[0], trawl_dir).split("/", 1)[0]
-        err.bucket_name = bucket_name
-        err.filename = original_refs[0]
-        raise
-    # product metadata extraction
-    mh = katsdpdata.met_handler.MetaDataHandler(solr_url, pm_extractor.product_type, prod_id, prod_id)
-    if not mh.get_prod_met(prod_id):
-        met = mh.create_core_met()
-    else:
-        met = mh.get_prod_met(prod_id)
-    if "CAS.ProductTransferStatus" in met and met["CAS.ProductTransferStatus"] == "RECEIVED":
-        err = katsdpdata.met_extractors.MetExtractorException(
-            "%s marked as RECEIVED, while trying to create new product." % (prod_id))
-        err.bucket_name = os.path.relpath(original_refs[0], trawl_dir).split("/", 1)[0]
-        raise err
-    # set metadata
-    met = mh.set_product_transferring(met)
-    # prepend the most common path to conform to hierarchical products
-    met_original_refs = list(original_refs)
-    met_original_refs.insert(0, os.path.dirname(os.path.commonprefix(original_refs)))
-    met = mh.add_ref_original(met, met_original_refs)
-    met = mh.add_prod_met(met, pm_extractor.metadata)
-    procs = parallel_upload(trawl_dir, boto_dict, original_refs)
-    transfer_list = []
-    for p in procs:
-        for r in p.result():
-            transfer_list.append(r)
-    # prepend the most common path to conform to hierarchical products
-    met_transfer_refs = list(transfer_list)
-    met_transfer_refs.insert(0, os.path.dirname(os.path.commonprefix(transfer_list)))
-    met = mh.add_ref_datastore(met, met_transfer_refs)
-    met = mh.set_product_received(met)
-    return met
-
-
 def timeit(func):
     """Taken from an example from the internet."""
     def wrapper(*args, **kwargs):
@@ -171,12 +111,12 @@ def parallel_upload(trawl_dir, boto_dict, file_list, **kwargs):
     procs = []
     with futures.ProcessPoolExecutor(max_workers=workers) as executor:
         for f in files:
-            procs.append(executor.submit(transfer_files, trawl_dir, boto_dict, f))
+            procs.append(executor.submit(transfer_files_to_s3, trawl_dir, boto_dict, f))
         executor.shutdown(wait=True)
     return procs
 
 
-def transfer_files(trawl_dir, boto_dict, file_list):
+def transfer_files_to_s3(trawl_dir, boto_dict, file_list):
     """Transfer file list to s3.
 
     Parameters
@@ -205,3 +145,118 @@ def transfer_files(trawl_dir, boto_dict, file_list):
         else:
             logger.debug("%s not deleted. Only uploaded %i of %i bytes." % (filename, res, file_size))
     return transfer_list
+
+
+def transfer_files_from_s3(target_dir, key_list):
+    """Transfer file list from s3.
+
+    Parameters
+    ----------
+    trawl_dir: string : The full path to the trawl directory
+    boto_dict: dict : parameter dict for boto connection.
+    file_list: list : a list of full path to files to transfer.
+
+    Returns
+    -------
+    transfer_list: list : a list of s3 URLs that where transfered.
+    """
+    transfer_list = []
+    for k in key_list:
+        try:
+            os.makedirs(os.path.join(os.path.abspath(target_dir), k.bucket.name, os.path.split(k.name)[0]))
+        except OSError:
+            import pdb; pdb.set_trace()
+        filename = os.path.join(os.path.abspath(target_dir), k.bucket.name, k.name)
+        k.get_contents_to_filename(filename)
+        transfer_list.append('file://{}'.format(filename))
+    return transfer_list
+
+
+def ingest_stream_product(trawl_dir, prod_id, original_refs, prod_met_extractor, solr_url, boto_dict):
+    """Ingest a product into the archive. This includes extracting and uploading
+    metadata and then moving the product into the archive.
+
+    Parameters
+    ----------
+    trawl_dir: string : full path to directory to trawl for ingest product.
+    prod_id: string : unique id for the product.
+    original_refs : list : list of product file(s).
+    product_met_extractor: class : a metadata extractor class.
+    solr_url: string : sorl endpoint for metadata queries and upload.
+
+    Returns
+    -------
+    met : dict : a metadata dictionary with uploaded key:value pairs.
+    """
+    try:
+        pm_extractor = prod_met_extractor(original_refs[0])
+        pm_extractor.extract_metadata()
+    except Exception as err:
+        bucket_name = os.path.relpath(original_refs[0], trawl_dir).split("/", 1)[0]
+        err.bucket_name = bucket_name
+        err.filename = original_refs[0]
+        raise
+    # product metadata extraction
+    mh = met_handler.MetaDataHandler(solr_url, pm_extractor.product_type, prod_id, prod_id)
+    if not mh.get_prod_met(prod_id):
+        met = mh.create_core_met()
+    else:
+        met = mh.get_prod_met(prod_id)
+    if "CAS.ProductTransferStatus" in met and met["CAS.ProductTransferStatus"] == "RECEIVED":
+        err = met_extractors.MetExtractorException(
+            "%s marked as RECEIVED, while trying to create new product." % (prod_id))
+        err.bucket_name = os.path.relpath(original_refs[0], trawl_dir).split("/", 1)[0]
+        raise err
+    # set metadata
+    met = mh.set_product_transferring(met)
+    # prepend the most common path to conform to hierarchical products
+    met_original_refs = list(original_refs)
+    met_original_refs.insert(0, os.path.dirname(os.path.commonprefix(original_refs)))
+    met = mh.add_ref_original(met, met_original_refs)
+    met = mh.add_prod_met(met, pm_extractor.metadata)
+    procs = parallel_upload(trawl_dir, boto_dict, original_refs)
+    transfer_list = []
+    for p in procs:
+        for r in p.result():
+            transfer_list.append(r)
+    # prepend the most common path to conform to hierarchical products
+    met_transfer_refs = list(transfer_list)
+    met_transfer_refs.insert(0, os.path.dirname(os.path.commonprefix(transfer_list)))
+    met = mh.add_ref_datastore(met, met_transfer_refs)
+    met = mh.set_product_received(met)
+    return met
+
+
+def get_stream_product(download_dir, s3_bucket, boto_dict):
+    download_dir = os.path.abspath(download_dir)
+    s3_conn = get_s3_connection(boto_dict)
+
+    if s3_bucket.startswith('s3://'):
+        bucket_name = os.path.split(s3_bucket)[1]
+    else:
+        bucket_name = s3_bucket
+    bucket = s3_conn.get_bucket(bucket_name)
+    for k in bucket:
+        download_file = os.path.join(download_dir, k.bucket.name, k.name)
+        if not os.path.isdir(os.path.split(download_file)[0]):
+            os.makedirs(os.path.split(download_file)[0])
+        k.get_contents_to_filename(download_file)
+
+
+def get_capture_block_buckets(capture_block_id, solr_url):
+    solr = pysolr.Solr(solr_url)
+    search_types = ' OR '.join('CAS.ProductTypeName:{}'.format(pt) for pt in ['MeerKATTelescopeProduct', 'MeerKATFlagProduct'])
+    return_fields = ', '.join(['CAS.ProductName, CAS.ReferenceDatastore'])
+    res = solr.search('CaptureBlockId:{} AND ({})'.format(capture_block_id, search_types),
+                      fl=return_fields)
+    s3_buckets = []
+    for d in res.docs:
+        s3_buckets.append('s3://{}'.format(d['CAS.ProductName']))
+        s3_buckets.append(d['CAS.ReferenceDatastore'][0])
+    return list(set(s3_buckets))
+
+
+def download_stream_products(download_dir, capture_block_id, solr_url, boto_dict):
+    bucket_names = get_capture_block_buckets(capture_block_id, solr_url)
+    for bn in bucket_names:
+        get_stream_product(download_dir, bn, boto_dict)
