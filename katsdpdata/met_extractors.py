aa095830542c9d603ff63b09b51c8ef143062918
Thomas Bennett <thomas@ska.ac.za>
2019-03-05 12:11:52 +0200

    Fix some strange exceptions relating to metadata extraction. This could be python3 related problems

--- katsdpdata/met_extractors.py
+++ katsdpdata/met_extractors.py
@@ -8,7 +8,6 @@ import katpoint
 import katdal
 import datetime
 import numpy
-from katcp import BlockingClient, Message
 from xml.etree import ElementTree
 
 class MetExtractorException(Exception):
@@ -94,7 +93,7 @@ class TelescopeProductMetExtractor(MetExtractor):
     def _extract_metadata_from_katdata(self):
         """Populate self.metadata: Get information using katdal"""
         self.metadata['Antennas'] = [a.name for a in self._katdata.ants]
-        self.metadata['CenterFrequency'] = str(self._katdata.channel_freqs[self._katdata.channels[-1]/2])
+        self.metadata['CenterFrequency'] = str(self._katdata.channel_freqs[int(len(self._katdata.channel_freqs)/2)])
         self.metadata['ChannelWidth'] = str(self._katdata.channel_width)
         self.metadata['MinFreq'] = str(min(self._katdata.freqs))
         self.metadata['MaxFreq'] = str(max(self._katdata.freqs) + self._katdata.channel_width)
@@ -113,7 +112,7 @@ class TelescopeProductMetExtractor(MetExtractor):
         self.metadata['NumFreqChannels'] = str(len(self._katdata.channels))
         self.metadata['Observer'] = self._katdata.observer
         self.metadata['RefAntenna'] = self._katdata.ref_ant
-        self.metadata['StartTime'] = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(self._katdata.start_time))
+        self.metadata['StartTime'] = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(float(self._katdata.start_time)))
         self.metadata['Targets'] = [t.name for t in self._katdata.catalogue.targets if t.name not in ['None', 'Nothing', 'azel', 'radec']]
 
         try:
@@ -186,14 +185,14 @@ class FileBasedTelescopeProductMetExtractor(TelescopeProductMetExtractor):
         if os.path.isfile(md5_filename):
             with open(md5_filename, 'r') as md5:
                  self.metadata['FileDigest']= md5.read().strip()
-                 print 'Digest is %s.' % self.metadata['FileDigest']
+                 print('Digest is %s.' % self.metadata['FileDigest'])
             os.remove(md5_filename)
         else:
-            print 'Calculating the md5 checksum for %s. This may take a while.' % (self.katfile)
+            print('Calculating the md5 checksum for %s. This may take a while.' % (self.katfile))
             p = subprocess.Popen(['md5sum', self.katfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
             if not p[1]:
                 self.metadata['FileDigest'] = p[0].split()[0]
-                print 'md5 checksum complete. Digest is %s.' % self.metadata['FileDigest']
+                print('md5 checksum complete. Digest is %s.' % self.metadata['FileDigest'])
 
     @staticmethod
     def factory(katfile):
@@ -268,7 +267,7 @@ class KAT7TelescopeProductMetExtractor(FileBasedTelescopeProductMetExtractor):
             self._extract_location_from_katdata()
             self._metadata_extracted = True
         else:
-           print "Metadata already extracted. Set the metadata_extracted attribute to False and run again." 
+           print("Metadata already extracted. Set the metadata_extracted attribute to False and run again." )
 
 class KatFileProductMetExtractor(KAT7TelescopeProductMetExtractor):
     def __init__(self, katdata):
@@ -327,7 +326,7 @@ class RTSTelescopeProductMetExtractor(FileBasedTelescopeProductMetExtractor):
             self._extract_location_from_katdata()
             self._metadata_extracted = True
         else:
-            print "Metadata already extracted. Set the metadata_extracted attribute to False and run again."
+            print("Metadata already extracted. Set the metadata_extracted attribute to False and run again.")
 
 class MeerKATAR1TelescopeProductMetExtractor(FileBasedTelescopeProductMetExtractor):
     """Used for extracting metadata for a MeerKATAR1TelescopeProduct.
@@ -395,7 +394,7 @@ class MeerKATAR1TelescopeProductMetExtractor(FileBasedTelescopeProductMetExtract
             self._extract_location_from_katdata()
             self._metadata_extracted = True
         else:
-            print "Metadata already extracted. Set the metadata_extracted attribute to False and run again."
+            print("Metadata already extracted. Set the metadata_extracted attribute to False and run again.")
 
 class MeerkatTelescopeTapeProductMetExtractor(TelescopeProductMetExtractor):
     """Used for extracting metadata for a MeerkatTelescopeTapeProduct. As well as extracting data
@@ -426,7 +425,7 @@ class MeerkatTelescopeTapeProductMetExtractor(TelescopeProductMetExtractor):
     def __init__(self, katdata):
         super(MeerkatTelescopeTapeProductMetExtractor, self).__init__(katdata)
         self._sensor_host = '192.168.6.233'
-        self._katcp_port = 5001
+        #self._katcp_port = 5001
         self._repository_path='/var/kat/data'
         #always set product_type after call to super
         self.product_type = 'MeerkatTelescopeTapeProduct'
@@ -434,12 +433,13 @@ class MeerkatTelescopeTapeProductMetExtractor(TelescopeProductMetExtractor):
     def _get_value_from_katsensor(self):
         """Populate self.metadata with information from a katcp request"""
         sensor_name_to_get = 'buffer_dir'
-        client = BlockingClient(self._sensor_host, self._katcp_port)
-        client.start()
-        client.wait_protocol()
-        reply, informs = client.blocking_request(Message.request("sensor-value", sensor_name_to_get))
-        client.stop()
-        client.join()
+        # client = BlockingClient(self._sensor_host, self._katcp_port)
+        # client.start()
+        # client.wait_protocol()
+        # reply, informs = client.blocking_request(Message.request("sensor-value", sensor_name_to_get))
+        # client.stop()
+        # client.join()
+        informs = None
         return informs[0].arguments[-1]
 
     def _extract_metadata_from_katsensor(self):
@@ -455,7 +455,7 @@ class MeerkatTelescopeTapeProductMetExtractor(TelescopeProductMetExtractor):
             self._extract_metadata_from_katsensor()
             self._metadata_extracted = True
         else:
-            print "Metadata already extracted. Set the metadata_extracted attribute to False and run again."
+            print("Metadata already extracted. Set the metadata_extracted attribute to False and run again.")
 
 class ReductionProductMetExtractor(MetExtractor):
     """A base class for handling reduction systems metadata extraction.
@@ -481,7 +481,7 @@ class ReductionProductMetExtractor(MetExtractor):
             self._extract_metadata_from_pickle()
             self._metadata_extracted = True
         else:
-            print "Metadata already extracted. Set the metadata_extracted attribute to False and run again."
+            print("Metadata already extracted. Set the metadata_extracted attribute to False and run again.")
 
     def _extract_metadata_from_pickle(self):
         with open(self._picklefile) as pickled_met:
@@ -540,7 +540,7 @@ class ObitReductionProductMetExtractor(MetExtractor):
             self._extract_metadata_from_votable()
             self._metadata_extracted = True
         else:
-            print "Metadata already extracted. Set the metadata_extracted attribute to False and run again."
+            print("Metadata already extracted. Set the metadata_extracted attribute to False and run again.")
 
     def _extract_metadata_from_votable(self):
         met = dict([[param.attrib['name'],param.attrib['value']] for param in self.project_data.getchildren() if param.tag == 'param'])
@@ -561,7 +561,7 @@ class BeaformerProductMetExtractor(MetExtractor):
         self.product_name = prod_name
 
     def _extract_locations(self):
-        print self.metadata
+        print(self.metadata)
         if 'KatpointTargets' in self.metadata and "StartTime" in self.metadata and 'Duration' in self.metadata:
             self.metadata["DecRa_path"]=[]
             self.metadata["DecRa"]=[]
@@ -576,7 +576,7 @@ class BeaformerProductMetExtractor(MetExtractor):
                         for ts in range(int(start), int(start + float(self.metadata['Duration'])), 10) + [start + float(self.metadata['Duration']),]:
                             self.metadata["DecRa_path"].append("%f,%f"%(target.radec(ts)[1],target.radec(ts)[0]))
                     except ValueError as e:
-                        print e
+                        print(e)
 
                 else:
                     self.metadata["DecRa"].append("%f,%f"%(target.radec()[1],target.radec()[0]))
@@ -589,7 +589,7 @@ class BeaformerProductMetExtractor(MetExtractor):
                 if len(self.metadata["ElAz_path"]) > 2000:
                     self.metadata["ElAz_path"] = self.metadata["ElAz_path"][::len(self.metadata["ElAz_path"])/2000 + 1] + [self.metadata["ElAz_path"][-1],]
         else:
-            print "Does not have required metadata"
+            print("Does not have required metadata")
             raise NotEnoughMetadata
 
 class NotEnoughMetadata(Exception):
@@ -609,22 +609,22 @@ class PulsarSearchProductMetExtractor(BeaformerProductMetExtractor):
         self.product_name = prod_name
     
     def extract_metadata(self):
-        print "extracting product type"
+        print("extracting product type")
         self._extract_metadata_product_type()
-        print "extracting fits"
+        print("extracting fits")
         self.extract_fits_header()
-        print "extracting location"
+        print("extracting location")
         self._extract_locations()
    
     def extract_fits_header(self):
-        print "fits header"
+        print("fits header")
         import pyfits
         data_files = os.listdir(self.product_name)
         count = 0
     
-        print data_files
+        print(data_files)
         while data_files[count][-2:] != 'sf':
-            print data_files[count][-2:]
+            print(data_files[count][-2:])
             count+=1
         data = pyfits.open("%s/%s"%(self.product_name,data_files[count]), memmap=True, ignore_missing_end=True)
         obs_info_file = open ("%s/obs_info.dat"%self.product_name)
@@ -677,11 +677,11 @@ class PulsarSearchProductMetExtractor(BeaformerProductMetExtractor):
                 target = katpoint.Target(t)
                 self.metadata['KatpointTargets'].append(target.description)
             except ValueError as e:
-                print e
+                print(e)
                 pass
                     
-        print "fits extracted"
-        print self.metadata
+        print("fits extracted")
+        print(self.metadata)
         self._metadata_extracted = True
 
 #input string of ra and dec in hours and return floats with the degree values
@@ -845,4 +845,4 @@ class CalibrationProductMetExtractor(MetExtractor):
             self.metadata['CaptureBlockId'] = self.product_name.split('/')[-1][:10]
             self._metadata_extracted = True
         else:
-            print "Metadata already extracted. Set the metadata_extracted attribute to False and run again."
+            print("Metadata already extracted. Set the metadata_extracted attribute to False and run again.")
