062a7bad873d683a06f90ae91d3b7fa843bb8c3b
Thomas Bennett <thomas@ska.ac.za>
2019-05-13 10:57:20 +0000

    PEP8 compliance - minus long lines

--- katsdptrawler/scripts/vis_crawler.py
+++ katsdptrawler/scripts/vis_crawler.py
@@ -12,12 +12,12 @@ logger = logging.getLogger("katsdpvistrawler.producttransfer")
 if __name__ == "__main__":
     katsdpservices.setup_logging()
     katsdpservices.setup_restart()
- 
-    parser = OptionParser(usage="vis_crawler.py\n"+
-                                "Example:\n"+
+
+    parser = OptionParser(usage="vis_crawler.py\n" +
+                                "Example:\n" +
                                 "vis_crawler.py [Options]")
- 
-    # Dir source 
+
+    # Dir source
     parser.add_option("--src-trawl-dir", default="/data/",
                       help="Top level directory [default = %default]")
     # S3 source
@@ -27,7 +27,7 @@ if __name__ == "__main__":
                       help="Source S3 gateway port [default = %default]")
     parser.add_option("--src-profile-name", default="default",
                       help="Source S3 profile name [default = %default]")
- 
+
     # S3 sink
     parser.add_option("--sink-host", default="localhost",
                       help="Sink S3 gateway host address [default = %default]")
@@ -35,29 +35,28 @@ if __name__ == "__main__":
                       help="Sink S3 gateway port [default = %default]")
     parser.add_option("--sink-profile-name", default="default",
                       help="Sink Source S3 profile name [default = %default]")
- 
+
     # Solr endpoint
     parser.add_option("--solr-endpoint", default="http://localhost:8983/solr/kat_core",
                       help="Solr endpoint for metadata extraction [default = %default]")
- 
+
     (options, args) = parser.parse_args()
- 
+
     logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
     logger.addHandler(console_handler)
- 
+
     src_type = args[0]
- 
+
     src = {}
     if src_type == 'dir':
-        src['config'] = {'trawl_dir':options.src_trawl_dir}
+        src['config'] = {'trawl_dir': options.src_trawl_dir}
         src['type'] = 'dir'
         crawler = katsdptrawler.s3transfer.LocalDirectoryCrawler(src)
     elif src_type == 's3':
-        src['config'] = {'host':options.src_host,
-                         'port':options.src_port,
-                         'profile_name':options.src_profile_name
-                        }
+        src['config'] = {'host': options.src_host,
+                         'port': options.src_port,
+                         'profile_name': options.src_profile_name}
         src['type'] = 's3'
         crawler = katsdptrawler.s3transfer.S3Crawler(src)
     else:
@@ -65,23 +64,31 @@ if __name__ == "__main__":
         sys.exit(0)
 
 sink = {}
-sink['config'] = {'host':options.sink_host, 'port':options.sink_port, 'profile_name':options.sink_profile_name}
+sink['config'] = {'host': options.sink_host,
+                  'port': options.sink_port,
+                  'profile_name': options.sink_profile_name}
 
 prods = crawler.list()
 
-vis_exec = 'scripts/vis_transfer.py %s' % (src['type']) 
+vis_exec = 'scripts/vis_transfer.py %s' % (src['type'])
 
 if src['type'] == 'dir':
     src_conf = '--src-trawl-dir %s' % (src['config']['trawl_dir'])
 elif src['type'] == 's3':
-    src_conf = '--src-host %s --src-port %s --src-profile-name %s'% (src['config']['host'], src['config']['port'], src['config']['profile_name'])
+    src_conf = '--src-host %s --src-port %s --src-profile-name %s' % (
+               src['config']['host'],
+               src['config']['port'],
+               src['config']['profile_name'])
 
-sink_conf = '--sink-host %s --sink-port %s --sink-profile-name %s' % (sink['config']['host'], sink['config']['port'], sink['config']['profile_name'])
+sink_conf = '--sink-host %s --sink-port %s --sink-profile-name %s' % (
+            sink['config']['host'],
+            sink['config']['port'],
+            sink['config']['profile_name'])
 
 solr_conf = '--solr-endpoint %s' % (options.solr_endpoint)
 
 print('='*80)
 for p in prods:
-   exec_str = ' '.join([vis_exec, src_conf, sink_conf, solr_conf, p['head'], p['stream']])
-   print(exec_str)   
+    exec_str = ' '.join([vis_exec, src_conf, sink_conf, solr_conf, p['head'], p['stream']])
+    print(exec_str)
 print('='*80)
