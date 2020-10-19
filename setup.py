c1ecdb0200b95221bc63bd5e0e0a5d7134ab04c9
Thomas Bennett <thomas@ska.ac.za>
2020-10-19 15:59:34 +0200

    Flake8 setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "katsdpdata",
-    description = "Data search and retrieval library for the MeerKAT project",
-    author = "Thomas Bennett, Chris Schollar",
-    author_email = "thomas@ska.ac.za, cschollar@ska.ac.za",
-    packages = find_packages(),
-    setup_requires = ["katversion"],
-    install_requires = [
-        "boto", "katdal", "katpoint", "katsdpservices", "katsdptelstate",
-        "numpy", "pysolr>=3.3.3"],
-    url = 'http://ska.ac.za/',
-    scripts = [
+    name="katsdpdata",
+    description="Data search and retrieval library for the MeerKAT project",
+    author="Thomas Bennett, Chris Schollar",
+    author_email="thomas@ska.ac.za, cschollar@ska.ac.za",
+    packages=find_packages(),
+    setup_requires=["katversion"],
+    install_requires=[
+        "boto==2.48.0", "katdal", "katpoint", "katsdpservices",
+        "katsdptelstate", "numpy", "pysolr>=3.3.3"],
+    url='http://ska.ac.za/',
+    scripts=[
         "scripts/tel_prod_met_extractor.py",
         "scripts/download_cbid_prods.py",
         "scripts/download_cbid_prods_maximum_plaid.py",
@@ -25,8 +25,8 @@ setup(
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering :: Astronomy"],
-    platforms = [ "OS Independent" ],
+    platforms=["OS Independent"],
     keywords="meerkat ska",
-    zip_safe = False,
-    use_katversion = True
+    zip_safe=False,
+    use_katversion=True
 )
